3dcc7e311b1b7c01973199c6511c3689256ba4b1
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-12-30 22:03:25 +0100

    Creation of the Explore section

--- src/components/ExploreCard.tsx
+++ src/components/ExploreCard.tsx
@@ -1,9 +1,48 @@
 'use client';
 
-const ExploreCard = () => (
-  <div>
-    Explore Card
-  </div>
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../utils/motion';
+import { ExploreProps } from '../types/props';
+
+const ExploreCard = ({ item, index, active, handleClick }: ExploreProps) => (
+  <motion.div
+    variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+    className={`relative h-[700px] min-w-[170px] flex items-center justify-center transition-[flex] duration-700 ease-out-flex cursor-pointer
+    ${
+      active === item.id ? 'flex-[10] lg:flex-[3.5]' : 'flex-[2] lg:flex-[0.5]'
+    }`}
+    onClick={() => handleClick(item.id)}
+  >
+    <Image
+      src={item.imgUrl}
+      alt={item.title}
+      width={item.width}
+      height={item.height}
+      className="absolute w-full h-full object-cover rounded-3xl"
+    />
+    {active === item.id ? (
+      <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-3xl">
+        <div className="flex justify-center items-center w-16 h-16 rounded-3xl glassmorphism mb-4">
+          <Image
+            src="/headset.svg"
+            alt="headset"
+            width={200}
+            height={200}
+            className="w-1/2 h-1/2 object-contain"
+          />
+        </div>
+        <p className="leading-5 text-white uppercase">Enter the metaverse</p>
+        <h3 className="mt-6 font-semibold text-white text-2xl sm:text-3xl">
+          {item.title}
+        </h3>
+      </div>
+    ) : (
+      <h3 className="absolute z-0 font-semibold text-white text-lg sm:text-2xl lg:bottom-20 lg:-rotate-90 lg:origin[0,0]">
+        {item.title}
+      </h3>
+    )}
+  </motion.div>
 );
 
 export default ExploreCard;
--- src/sections/Explore.tsx
+++ src/sections/Explore.tsx
@@ -1,28 +1,44 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import Image from 'next/image';
 import { useState } from 'react';
 import { TypingText, ExploreCard, TitleText } from '../components';
 import { exploreWorlds } from '../constants';
+import { ExplorerImage } from '../types';
 import { staggerContainer } from '../utils/motion';
 
-const Explore = () => (
-  <section className="py-12 px-6 sm:p-16" id="explore">
-    <motion.div
-      variants={staggerContainer()}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center"
-    >
-      <TypingText title="| The World" textStyles="text-center" />
-      <TitleText textStyles="text-center">
-        Choose the world you want
-        <br className="hidden md:block" /> to explore
-      </TitleText>
-    </motion.div>
-  </section>
-);
+const Explore = () => {
+  const [active, setActive] = useState('world-2');
+
+  return (
+    <section className="py-12 px-6 sm:p-16" id="explore">
+      <motion.div
+        variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center"
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText textStyles="text-center">
+          Choose the world you want
+          <br className="hidden md:block" /> to explore
+        </TitleText>
+
+        <div className="w-full mt-12 flex gap-5 flex-col lg:flex-row min-h-[70vh]">
+          {exploreWorlds.map((item: ExplorerImage, index: number) => (
+            <ExploreCard
+              key={item.id}
+              index={index}
+              item={item}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default Explore;
