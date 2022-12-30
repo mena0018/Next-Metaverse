444f8e9c8a73e5981ec04f35986a1c0f87cbf384
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-12-30 19:16:54 +0100

    Creation of the Custom Text component

--- src/components/CustomTexts.tsx
+++ src/components/CustomTexts.tsx
@@ -1,9 +1,39 @@
 'use client';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from 'framer-motion';
+import { textContainer, textVariant2 } from '../utils/motion';
+import { ReactNode } from 'react';
+
+type TypingProps = {
+  title: string;
+  textStyles: string;
+};
+
+type TitleProps = {
+  children: ReactNode;
+  textStyles: string;
+};
+
+export const TypingText = ({ title, textStyles }: TypingProps) => (
+  <motion.p
+    variants={textContainer}
+    className={`${textStyles} text-sm text-secondary-white`}
+  >
+    {Array.from(title).map((letter: string, index: number) => (
+      <motion.span key={index} variants={textVariant2}>
+        {letter}
+      </motion.span>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
+export const TitleText = ({ children, textStyles }: TitleProps) => (
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`${textStyles} mt-4 font-bold text-white text-center text-[40px] md:text-[64px]`}
+  >
+    {children}
+  </motion.h2>
 );
