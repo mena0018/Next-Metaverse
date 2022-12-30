df1d5bc324d5d525e25252bbb41a523a3bd96364
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-12-30 19:17:14 +0100

    Creation of the Explore section

--- src/sections/Explore.tsx
+++ src/sections/Explore.tsx
@@ -1,5 +1,28 @@
-"use client";
+'use client';
 
-const Explore = () => <section>Explore section</section>;
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import { useState } from 'react';
+import { TypingText, ExploreCard, TitleText } from '../components';
+import { exploreWorlds } from '../constants';
+import { staggerContainer } from '../utils/motion';
+
+const Explore = () => (
+  <section className="py-12 px-6 sm:p-16" id="explore">
+    <motion.div
+      variants={staggerContainer()}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center"
+    >
+      <TypingText title="| The World" textStyles="text-center" />
+      <TitleText textStyles="text-center">
+        Choose the world you want
+        <br className="hidden md:block" /> to explore
+      </TitleText>
+    </motion.div>
+  </section>
+);
 
 export default Explore;
