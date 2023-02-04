3f29e7b5c4436c453cb123fa6e007a74df23332a
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2023-02-04 19:45:28 +0100

    Modification of the gradient

--- app/page.tsx
+++ app/page.tsx
@@ -17,20 +17,20 @@ const Page = () => (
 
     <div className="relative">
       <About />
-      <div className="gradient-03 z-0" />
+      <div className="gradient-03" />
       <Explore />
     </div>
 
     <div className="relative">
       <GetStarted />
-      <div className="gradient-04 z-0" />
+      <div className="gradient-04" />
       <WhatsNew />
     </div>
     <World />
 
     <div className="relative">
       <Insights />
-      <div className="gradient-04 z-0" />
+      <div className="gradient-04" />
       <Feedback />
     </div>
 
